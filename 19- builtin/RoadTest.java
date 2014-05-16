4a5fd05014e6da28e5c383f3328aa5a10a70df62
emilyliao97 <liao.emily022@gmail.com>
2014-05-16 08:40:41 -0400

    pt ii

---
+++ 19- builtin/RoadTest.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class RoadTest {
+
+    long start = System.currentTimeMillis();
+
+    TreeMap branch = new TreeMap();
+    branch.put("a", 1);
+    branch.put("b", 2);
+    branch.put("c", 3);
+
+    long time = System.currentTimeMillis();
+    time -= start;
+
+    start = System.currentTimeMillis();
+
+    HashMap hashbrown = new HashMap();
+
+}
