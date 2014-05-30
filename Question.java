40f91dff4099279f23469f327cf0a9b1bee0e210
emilyliao97 <liao.emily022@gmail.com>
2014-05-30 00:32:00 -0400

    Update Question.java

--- Question.java
+++ Question.java
@@ -7,6 +7,27 @@ public class Question {
   
     Question q1 = new Question();
     
+    File text;
+    Scanner s = new Scanner(text);
+    
+    int lineNumber = 1;
+    
+    while(scnr.hasNextLine()){
+      String line = scnr.nextLine();
+      System.out.println("line " + lineNumber + " :" + line);
+      lineNumber++;
+    }       
+      
+    
+    PrintWriter out=null;
+	  try {
+	    out = new PrintWriter(new BufferedWriter(new FileWriter("test.out")));
+	  } 
+	  catch (Exception e){};
+    out.print ("HELLO");
+	  out.println (" World!");
+	  out.close ();
+
   }
 
 }
